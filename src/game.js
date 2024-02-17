d8925880ea56ce0992db38cb8067171a7170e43b
Ashutosh <ashu.ashutoshh@gmail.com>
2024-02-17 22:13:06 +0530

    updated readme and updated game.js, Fixed the bug where in console player name was not printing

--- src/game.js
+++ src/game.js
@@ -1,34 +1,41 @@
-class Player {
-    constructor(health, strength, attack) {
-        this.health = health;
-        this.strength = strength;
-        this.attack = attack;
+class Game {
+    constructor(playerA, playerB, diceSides) {
+        this.playerA = playerA;
+        this.playerB = playerB;
+        this.diceSides = diceSides;
+        this.currentPlayer = (playerA.health < playerB.health) ? playerA : playerB;
+        this.opponent = (this.currentPlayer === playerA) ? playerB : playerA;
     }
 
-    attackRoll(sides) {
-        return Math.floor(Math.random() * sides) + 1; // 1-sides random roll
-    }
-
-    defendRoll(sides) {
-        return Math.floor(Math.random() * sides) + 1; // 1-sides random roll
-    }
-
-    performAttack(opponent, diceSides) {
-        const attackRoll = this.attackRoll(diceSides);
-        const damageDealt = attackRoll * this.attack;
-
-        const defendRoll = opponent.defendRoll(diceSides);
-        const damageDefended = defendRoll * opponent.strength;
-
+    playTurn() {
+        const attackRoll = this.currentPlayer.attackRoll(this.diceSides);
+        const defendRoll = this.opponent.defendRoll(this.diceSides);
+    
+        const damageDealt = attackRoll * this.currentPlayer.attack;
+        const damageDefended = defendRoll * this.opponent.strength;
+    
         const damageTaken = Math.max(damageDealt - damageDefended, 0);
-        opponent.health -= damageTaken;
+        this.opponent.health -= damageTaken;
+    
+        console.log(`${this.currentPlayer.constructor.name} ${this.currentPlayer === this.playerA ? 'A' : 'B'} attacks ${this.opponent.constructor.name} ${this.opponent === this.playerA ? 'A' : 'B'}!`);
+        console.log(`Attack Roll: ${attackRoll}`);
+        console.log(`Defend Roll: ${defendRoll}`);
+        console.log(`Damage Dealt: ${damageDealt}`);
+        console.log(`Damage Defended: ${damageDefended}`);
+        console.log(`${this.opponent.constructor.name}'s Health: ${this.opponent.health}\n`);
+    
+        // Swap players for the next turn
+        [this.currentPlayer, this.opponent] = [this.opponent, this.currentPlayer];
+    }
 
-        return {
-            damageDealt,
-            damageDefended,
-            damageTaken
-        };
+    playGame() {
+        while (this.playerA.health > 0 && this.playerB.health > 0) {
+            this.playTurn();
+        }
+
+        const winner = (this.playerA.health > 0) ? this.playerA : this.playerB;
+        console.log(`${winner.constructor.name} wins the game!`);
     }
 }
 
-module.exports = Player;
+module.exports = Game;
---
+++ src/index.js
@@ -0,0 +1,43 @@
+const Player = require('./player');
+const Game = require('./game');
+const readline = require('readline');
+
+// Function to create a readline interface
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Function to prompt user for player attributes
+const getPlayerInput = (playerName, callback) => {
+    rl.question(`Enter attributes for ${playerName} (health, strength, attack): `, (input) => {
+        const [health, strength, attack] = input.split(' ').map(val => parseInt(val));
+        callback({ health, strength, attack });
+    });
+};
+
+// Function to prompt user for number of sides on the dice
+const getDiceSides = (callback) => {
+    rl.question('Enter number of sides on the dice: ', (input) => {
+        const sides = parseInt(input);
+        callback(sides);
+    });
+};
+
+// Get player attributes
+getPlayerInput('Player A', (playerAAttributes) => {
+    getPlayerInput('Player B', (playerBAttributes) => {
+        getDiceSides((diceSides) => {
+            // Create players and start the game
+            const playerA = new Player(playerAAttributes.health, playerAAttributes.strength, playerAAttributes.attack);
+            const playerB = new Player(playerBAttributes.health, playerBAttributes.strength, playerBAttributes.attack);
+            const game = new Game(playerA, playerB, diceSides);
+            
+            // Start the game
+            game.playGame();
+
+            // Close the readline interface
+            rl.close();
+        });
+    });
+});
